0d111c5c90c21a59d31ebf4094eabbe17ea3a7d8
Ash709 <ashlinmishra2003@gmail.com>
2025-07-25 19:46:23 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -3,7 +3,7 @@ import mysql.connector
 def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
-        user="root",       # replace with your MySQL username
-        password="ASH@1234562003",   # replace with your MySQL password
-        database="tastynuts"        # make sure this DB and 'bookings' table exist
+        user="root",       
+        password="ASH@1234562003",
+        database="tastynuts"        
     )
