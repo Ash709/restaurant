ecbf26e8a4f803b897a050a1003e5ba15c071438
Ash709 <ashlinmishra2003@gmail.com>
2026-02-25 22:59:54 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -2,10 +2,9 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",               # use localhost instead of 127.0.0.1
-        port=3306,
         user="root",
         password="ASH@1234562003",
         database="tastynuts",
-        
+        auth_plugin='mysql_native_password',
+        unix_socket=r"\\.\pipe\MySQL80"
     )
