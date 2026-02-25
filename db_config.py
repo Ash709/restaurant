a78bda997122a8215db4fa8e1b0242c9583736a5
Ash709 <ashlinmishra2003@gmail.com>
2026-02-25 15:58:50 +0530

    Refactor database connection into a function

--- db_config.py
+++ db_config.py
@@ -1,14 +1,8 @@
 import mysql.connector
 
-try:
-    conn = mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="ASH@1234562003",
-        database="tastynuts"
-         port=3306
-    )
-    print("Connected successfully!")
-    conn.close()
-except Exception as e:
-    print("Connection failed:", e)
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",       
+        password="ASH@1234562003",   
+        database="tastynuts"        
