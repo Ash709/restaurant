772065eb90fc12f2612adcaa631850aca6bebc1d
Ash709 <ashlinmishra2003@gmail.com>
2026-02-25 23:32:15 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -2,10 +2,10 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="127.0.0.1"
+        host="127.0.0.1",               # use IPv4 to match root@127.0.0.1
+        port=3306,
         user="root",
         password="ASH@1234562003",
         database="tastynuts",
-        auth_plugin='mysql_native_password',
-        unix_socket=r"\\.\pipe\MySQL80"
+        auth_plugin='mysql_native_password'
     )
