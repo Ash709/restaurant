0f68da60b00ae1fc62df57f6e2d11f1ac3a009e6
Ash709 <ashlinmishra2003@gmail.com>
2026-02-25 17:00:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ from db_config import get_db_connection
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
 
 @app.route("/book", methods=["POST"])
 def book():
@@ -24,7 +20,4 @@ def book():
 
     except Exception as e:
         return f"Something went wrong: {e}", 500
-import os
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000))) 
