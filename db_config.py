8ab1ee890f9aefce737f26d33202f270bd788f1b
Ash709 <ashlinmishra2003@gmail.com>
2026-02-25 18:49:49 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -2,5 +2,4 @@ import os
 import psycopg2
 
 def get_db_connection():
-    conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
-    return conn
+    return psycopg2.connect(os.environ.get("DATABASE_URL"))
