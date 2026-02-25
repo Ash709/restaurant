f02bf590115494c456a1ba1272243af2b697333a
Ash709 <ashlinmishra2003@gmail.com>
2026-02-25 21:30:56 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="127.0.0.1",       
+        host="localhost",       
         user="root",
         password="ASH@1234562003",
         database="tastynuts"
