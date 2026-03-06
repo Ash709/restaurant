9156d1f03d342308e8f9bd3f43a394f558bca3a6
Ash709 <ashlinmishra2003@gmail.com>
2026-03-06 20:57:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-
 from flask import Flask, render_template, request
-from db_config import get_db_connection
-from mysql.connector import Error
+from mysql.connector import connect, Error
+from db_config import DB_CONFIG, SOCKET_PATH  # we'll define these in db_config.py
 
 app = Flask(__name__)
+
+def get_db_connection_safe():
+    """Try TCP connection first, fallback to Unix socket if TCP fails."""
+    try:
+        return connect(
+            host=DB_CONFIG["host"],
+            user=DB_CONFIG["user"],
+            password=DB_CONFIG["password"],
+            database=DB_CONFIG["database"],
+            port=DB_CONFIG.get("port", 3306)
+        )
+    except Error as e:
+        if e.errno == 2003:  # Can't connect to MySQL server
+            # fallback to Unix socket
+            return connect(
+                user=DB_CONFIG["user"],
+                password=DB_CONFIG["password"],
+                database=DB_CONFIG["database"],
+                unix_socket=SOCKET_PATH
+            )
+        else:
+            raise
 
 @app.route("/")
 def home():
@@ -18,7 +39,7 @@ def book():
         time = request.form["time"]
         guests = request.form["guests"]
 
-        conn = get_db_connection()
+        conn = get_db_connection_safe()
         cursor = conn.cursor()
 
         query = """
