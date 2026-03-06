d4fac5a4c7c6a62cf9ccd35aac7ab20e798b3b57
Ash709 <ashlinmishra2003@gmail.com>
2026-03-06 20:23:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ app = Flask(__name__)
 def get_db_connection():
     try:
         conn = mysql.connector.connect(
-            host="localhost",
-            port=3306,
-            user="root",
-            password="ASH@1234562003",
-            database="tastynuts",
-            auth_plugin='mysql_native_password',
-            connection_timeout=5
+        host="127.0.0.1",
+        port=3306,
+        user="root",
+        password="ASH@1234562003",
+        database="tastynuts",
+        auth_plugin='mysql_native_password',
+        connection_timeout=5
+)
         )
         return conn
     except mysql.connector.Error as err:
