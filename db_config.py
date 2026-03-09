6572665ab59502b0c692f017bd78ac5d8b22c3cb
Ash709 <ashlinmishra2003@gmail.com>
2026-03-09 20:53:44 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,16 +1,15 @@
-import mysql.connector
-
 def get_db_connection():
     try:
         conn = mysql.connector.connect(
-            host="localhost",
+            host="127.0.0.1",
             user="root",
             password="ASH@1234562003",
             database="tastynuts",
             port=3306
         )
+        print("Database Connected Successfully")
         return conn
 
     except mysql.connector.Error as err:
-        print("MySQL Error:", err)
+        print("MySQL Connection Error:", err)
         return None
