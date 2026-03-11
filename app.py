fa8adf2a933409b997e953510248a237f48117aa
Ash709 <ashlinmishra2003@gmail.com>
2026-03-11 18:07:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,5 +45,5 @@ def book():
 import os
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
-    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
