b570294e7e8bc081845f33f6eb612f5422305eb4
Ash709 <ashlinmishra2003@gmail.com>
2026-03-11 14:38:53 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -3,14 +3,13 @@ import mysql.connector
 def get_db_connection():
     try:
         conn = mysql.connector.connect(
-            host="localhost",
+            host="mysql.railway.internal",
+            port=3306,
             user="root",
-            password="ASH@1234562003",
-            database="tastynuts",
-            port=3306
+            password="ckzQadVYiVGWFNlKpPRiXrzrVDqPOLDM",
+            database="railway"
         )
         return conn
-
     except mysql.connector.Error as err:
-        print("MySQL Error:", err)
+        print("Database Error:", err)
         return None
