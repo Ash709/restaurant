a5ed1a2c87bae9c3820f9543890d6688dafcf4be
Ash709 <ashlinmishra2003@gmail.com>
2026-04-05 13:53:48 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -2,16 +2,10 @@ import mysql.connector
 import os
 
 def get_db_connection():
-    try:
-        conn = mysql.connector.connect(
-            host=os.getenv("DB_HOST", "centerbeam.proxy.rlwy.net"),
-            port=int(os.getenv("DB_PORT", 16458)),
-            user=os.getenv("DB_USER", "root"),
-            password=os.getenv("DB_PASSWORD", "ckzQadVYiVGWFNlKpPRiXrzrVDqPOLDM"),
-            database=os.getenv("DB_NAME", "railway")
-        )
-        return conn
-
-    except mysql.connector.Error as err:
-        print("MySQL Error:", err)
-        return None
+    return mysql.connector.connect(
+        host=os.environ.get("DB_HOST"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME"),
+        port=int(os.environ.get("DB_PORT", 3306))
+    )
